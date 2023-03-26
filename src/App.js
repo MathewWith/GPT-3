ba1ca9beb8c52354c552281ac50240a593ebfd1b
Matthew Ovsyannikov <v.pg.98@mail.ru>
2023-03-26 15:15:05 +0300

    add the main architecture and common css styles

--- src/App.js
+++ src/App.js
@@ -1,9 +1,23 @@
 import React from "react"
 
+import { Header, Footer, Possibility, Blog, WhatGPT3, Features } from "./containers";
+import { CTA, Brand, Navbar } from './components';
+import './App.css';
+
 export const App = () => {
     return (
-        <div>
-            App
+        <div className="App">
+            <div className="gradient__bg">
+                <Navbar />
+                <Header />
+            </div>
+            <Brand />
+            <WhatGPT3 />
+            <Features />
+            <Possibility />
+            <CTA />
+            <Blog />
+            <Footer />
         </div>
     )
 }
